22f7af590c2cd1e43f391ec16e5c29a760995904
Nulong <54779467+andy010629@users.noreply.github.com>
2023-02-12 21:50:48 +0800

    Create docker-image.yml and change get_fake_teacher() type to Teacher and add /test/course search query (#2)
    * split route,add course model,add /test/course
    
    * Create docker-image.yml
    
    * change get_fake_teacher() type to Teacher,add /test/course search query

--- gopen_backend/routers/tests.py
+++ gopen_backend/routers/tests.py
@@ -1,8 +1,20 @@
 from fastapi import APIRouter
 from fake_data.fake_data import get_fake_courses
+from typing import List
+
 
 router = APIRouter()
 
 @router.get('/course')
-def get_course():
-   return get_fake_courses(20)
+def get_course(search:str="") -> list:
+   courses =  get_fake_courses(20)
+   filter_courses = []
+   for course in courses:
+      if search in course.id or search in course.name or search in course.class_id:
+         filter_courses.append(course)
+      else:
+         for teacher in course.teacher_list:
+            if search in teacher.name:
+               filter_courses.append(teacher)
+   return filter_courses
+

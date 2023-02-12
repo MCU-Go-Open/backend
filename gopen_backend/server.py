bc6a59a300eea1b24da29793ca332cd955e65ae6
Andy Lin <andy010629@gmail.com>
2023-02-12 23:10:26 +0800

    add testing/github flow with python-app

--- gopen_backend/server.py
+++ gopen_backend/server.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
+
+import os,sys
+sys.path.append(os.path.dirname(__file__))
+
 from routers import tests,auth,comment,course
+
 
 app = FastAPI()
 app.include_router(tests.router,prefix="/test",tags=["test"])
@@ -8,9 +13,9 @@ app.include_router(course.router,prefix="/course",tags=["course"])
 app.include_router(auth.router,prefix="/auth",tags=["auth"])
 app.include_router(comment.router,prefix="/comment",tags=["comment"])
 
-
 @app.get("/PING")
 def ping() -> str:
    return "PONG"
 
-uvicorn.run(app, host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+   uvicorn.run(app, host='0.0.0.0', port=8080)
